0f824ed9e1c21433a1c5b4a702815020c7462f86
MahaAlchommari <mahaalchommari@gmail.com>
2023-09-29 11:17:21 +0300

    calculator

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+const cont = document.querySelector(".cont");
+const buttons = document.querySelectorAll("button");
+const char = ["%", "*", "/", "-", "+", "="];
+let output = "";
+const calculate = (btnValue) => {
+  if (btnValue === "=" && output !== "") {
+    output = eval(output.replace("%", "/100"));
+  } else if (btnValue === "AC") {
+    output = "";
+  } else if (btnValue === "DEL") {
+    output = output.toString().slice(0, -1);
+  } else {
+    if (output === "" && char.includes(btnValue)) return;
+    output += btnValue;
+  }
+  cont.value = output;
+};
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => calculate(e.target.dataset.value));
+});
